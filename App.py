58fe0c00a7312e51bc7d34a2471915c04f651263
phatn <pngo5@student.gsu.edu>
2020-12-22 12:15:58 -0500

    Adding swagger

--- App.py
+++ App.py
@@ -6,11 +6,17 @@ Created on 12/21/2020
 """
 
 from flask import Flask,request
+import flasgger
+from flasgger import Swagger
 import pandas as pd
 import numpy as np
 import pickle
+
 #-------------------------------------------
 app =Flask(__name__)
+Swagger(app)
+
+
 
 pickle_in=open('classifier.pkl','rb')
 classifier=pickle.load(pickle_in)
@@ -26,24 +32,64 @@ def welcome():
  
 #api 1 based on values (using postman to test out API GET)
 #Test pass
-@app.route('/predict')
+@app.route('/predict',methods=["Get"])
 def predict_note_authentication():
-   variance=request.args.get('variance')
-   skewness=request.args.get('skewness')
-   curtosis=request.args.get('curtosis')
-   entropy=request.args.get('entropy')
-   prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
-   return "The predicited valyes is"+ str(prediction)
+    
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+    
+    """
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    return "The predicited valyes is"+ str(prediction)
     
 
 #api 2
 #Creating a post method that will take in a csv files, and return the predictions 
 @app.route('/predict_file',methods=["POST"])
 def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+    
+    """
     df_test=pd.read_csv(request.files.get("file"))
     prediction=classifier.predict(df_test)
-    return "The predicted values for the csv is"+ str(prediction)
-
+    return str(list(prediction))
+# VERY WEIRD SWAGGER BUG. Sometimes it works, and other time its doesn't.
 # moving on to postman to test out api on postman (POST)
 # Test result from the file testfile is The predicted values for the csv is[0 0 0 0 1 1 1 1 1]  
 
